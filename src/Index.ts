0ab3e97aad49394544130d52d35770b528bf8a6f
Phillip Hoff <pmhoff@Phillips-MacBook-Pro.local>
2017-04-08 16:43:21 -0700

    Allow configuration of values.

--- src/Index.ts
+++ src/Index.ts
@@ -1,3 +1,26 @@
 import * as AlexaAppServer from 'alexa-app-server';
+import * as process from 'process';
 
-AlexaAppServer.start({ port: 8080 });
+function parseBoolean(value: string | undefined, defaultValue: boolean): boolean {
+    if (value) {
+        const normalizedValue = value.toLowerCase();
+
+        if (normalizedValue === 'true') {
+            return true;
+        }
+        else if (normalizedValue === 'false') {
+            return false;
+        }
+        else {
+            throw new Error('A boolean value must be \'true\' or \'false\'.');
+        }
+    }
+
+    return defaultValue;
+}
+
+AlexaAppServer.start({
+    debug: parseBoolean(process.env.SPU_DEBUG, false),
+    port: process.env.PORT || 8080,
+    verify: parseBoolean(process.env.SPU_VERIFY, true),
+});
